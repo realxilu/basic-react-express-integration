402a65dc1b7d5c8a1cc058d44122fdb427bda995
realxilu <69646469+realxilu@users.noreply.github.com>
2021-08-08 20:49:51 -0700

    t

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const app = express()
 
 const port = 5000
 
-app.listen(port, () => {
-  console.log('server has been started!')
-})
+app.listen(port)
 
 app.get('/hello', (req, res) => {
-  res.json({ msg: 'hello from server' })
+  res.json({ msg: 'this is from the backend' })
 })
